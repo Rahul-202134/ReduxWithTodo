dfef3637536fb13ab9c71c58c269b6cbef386a2a
Rahul Kumar <rahulkumar202134@gmail.com>
2024-12-01 12:40:48 +0530

    commit

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../Features/Todo/todoSlice';
+
+const store = configureStore({
+    reducer: todoReducer
+})
+export default store
